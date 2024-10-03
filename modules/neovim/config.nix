1846859dd4a055817e083799c378fcaf995af4fa
Scott Murray <scott@murray.kiwi>
2024-10-03 22:14:08 +1300

    fix nvim config url

--- modules/neovim/config.nix
+++ modules/neovim/config.nix
@@ -8,7 +8,7 @@
     neovimSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
       if [ ! -d $HOME/.config/nvim/.git ]; then
         $DRY_RUN_CMD mkdir -p $HOME/.config/nvim
-        $DRY_RUN_CMD ${pkgs.git}/bin/git clone git@github.com/scott-the-programmer/nvim-config.git $HOME/.config/nvim
+        $DRY_RUN_CMD ${pkgs.git}/bin/git clone git@github.com:scott-the-programmer/nvim-config.git $HOME/.config/nvim
       else
         $DRY_RUN_CMD ${pkgs.git}/bin/git -C $HOME/.config/nvim pull
       fi
