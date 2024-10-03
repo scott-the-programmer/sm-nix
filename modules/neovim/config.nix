1b1c7f4ddf7b505d34564ac6a9b1c657bb742858
Scott Murray <scott@murray.kiwi>
2024-10-03 22:28:39 +1300

    use builtins.fetchGit to get nvim config

--- modules/neovim/config.nix
+++ modules/neovim/config.nix
@@ -1,17 +1,19 @@
-{ pkgs, lib, ... }: 
+{ pkgs, lib, ... }:
+
+let
+  nvimConfig = builtins.fetchGit {
+    url = "git@github.com:scott-the-programmer/nvim-config.git";
+    ref = "main";
+    rev = "96995e89b3068b6dd6f660dada49c280a75e9d55"; 
+  };
+in
 {
   home.packages = with pkgs; [
     neovim
   ];
 
-  home.activation = {
-    neovimSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if [ ! -d $HOME/.config/nvim/.git ]; then
-        $DRY_RUN_CMD mkdir -p $HOME/.config/nvim
-        $DRY_RUN_CMD ${pkgs.git}/bin/git clone git@github.com:scott-the-programmer/nvim-config.git $HOME/.config/nvim
-      else
-        $DRY_RUN_CMD ${pkgs.git}/bin/git -C $HOME/.config/nvim pull
-      fi
-    '';
+  home.file.".config/nvim" = {
+    source = nvimConfig;
+    recursive = true;
   };
 }
